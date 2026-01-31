f8d7384c2dd83baa8cde1174259fbf041d813e0c
dani0907 <dy696786@gmail.com>
2026-01-30 22:32:32 -0800

    fixed Readme before develop

--- kanban_react/src/pages/mainPage.jsx
+++ kanban_react/src/pages/mainPage.jsx
@@ -3,41 +3,50 @@ function MainPage(){
     <div className="mainInner">
       <h1>Kanban board</h1>
       <div className="kanbanContainer">
-        <div className="kanbanBox">
-          <h2>To do</h2>
-          <div className="kanbanItemBox">
-            <div className="kanbanItem">
-              <h3>kanban title</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, excepturi?</p>
-              <span>created on</span>
+        <div className="kanbanEnterBox">
+          <h2>Inbox</h2>
+          <div className="kanbanInputListBox">
+            <button>add a card</button>
+          </div>
+        </div>
+        <div className="kanbanListBox">
+          <div className="kanbanBox">
+            <h2>To do</h2>
+            <div className="kanbanItemBox">
+              <div className="kanbanItem">
+                <h3>kanban title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, excepturi?</p>
+                <span>created on</span>
+              </div>
+              <div className="kanbanItem">
+                <h3>kanban title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, excepturi?</p>
+                <span>created on</span>
+              </div>
             </div>
-            <div className="kanbanItem">
-              <h3>kanban title</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, excepturi?</p>
-              <span>created on</span>
+          </div>
+          <div className="kanbanBox">
+            <h2>In progress</h2>
+            <div className="kanbanItemBox">
+              <div className="kanbanItem">
+                <h3>kanban title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, excepturi?</p>
+                <span>created on</span>
+              </div>
+            </div>
+          </div>
+          <div className="kanbanBox">
+            <h2>Done</h2>
+            <div className="kanbanItemBox">
+              <div className="kanbanItem">
+                <h3>kanban title</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, excepturi?</p>
+                <span>created on</span>
+              </div>
             </div>
           </div>
         </div>
-        <div className="kanbanBox">
-          <h2>In progress</h2>
-          <div className="kanbanItemBox">
-            <div className="kanbanItem">
-              <h3>kanban title</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, excepturi?</p>
-              <span>created on</span>
-            </div>
-          </div>
-        </div>
-        <div className="kanbanBox">
-          <h2>Done</h2>
-          <div className="kanbanItemBox">
-            <div className="kanbanItem">
-              <h3>kanban title</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, excepturi?</p>
-              <span>created on</span>
-            </div>
-          </div>
-        </div>
+        
       </div>
     </div>
   )
